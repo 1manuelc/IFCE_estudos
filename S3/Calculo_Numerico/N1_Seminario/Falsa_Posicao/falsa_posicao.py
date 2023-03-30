de3a9565aad4cb8577ab6982d88dbca009e4c14d
Manuel Carlos <manuelwn21@gmail.com>
2023-03-30 16:04:22 -0300

    feat: falsa_posicao
    adicionado metodo da falsa posicao e codigo do metodo da bissecao refatorado

--- S3/Calculo_Numerico/N1_Seminario/Falsa_Posicao/falsa_posicao.py
+++ S3/Calculo_Numerico/N1_Seminario/Falsa_Posicao/falsa_posicao.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot
 def f(x):
     return m.sin(x) - e ** x
 
-def bissecao(f, a, b, epsilon, maxIteracoes):
+def falsa_posicao(f, a, b, epsilon, maxIteracoes):
     fa = f(a)
     fb = f(b)
 
@@ -19,41 +19,49 @@ def bissecao(f, a, b, epsilon, maxIteracoes):
 
     # checando se a funcao muda de sinal no intervalo
     if(fa * fb > 0):
-        return(True, k, None, coord)
+        return(True, 0, None, coord)
     
     intervaloAtual = abs(b - a)
-    x = (a + b) / 2
-    fx = f(x)
+
+    coord.append((0, a, fa, b, fb, 0, 0, intervaloAtual))
 
     # checando se a diferenca do intervalo ja nao 
     # esta proxima o suficiente da tolerancia
     if(intervaloAtual <= epsilon):
-        return(False, k, x, coord)
+        return(False, 0, a, coord)
+
+    # checando se a raiz nao esta nas extremidades a e b
+    if(abs(fa) <= epsilon):
+        return(True, 0, a, coord)
     
-    coord.append((0, a, fa, b, fb, x, fx, intervaloAtual))
+    if(abs(fb) <= epsilon):
+        return(True, 0, b, coord)
+    
+    for k in range(1, maxIteracoes + 1):
+        # calculando xk como secante
+        x = ((a * fb) - (b * fa)) / (fb - fa)
+        fx = f(x)
 
-    k = 0
+        coord.append((k, a, fa, b, fb, x, fx, intervaloAtual))
 
-    while(k <= maxIteracoes):
+        # checando se o valor de fx ja nao esta proximo
+        # o suficiente da tolerancia
+        if(abs(fx) <= epsilon):
+            return(False, k, x, coord)
+        
         if (fa * fx > 0):
             a = x
             fa = fx
         else:
             b = x
             fb = fx
-        
-        intervaloAtual = abs(b - a)
-        x = (a + b) / 2
-        fx = f(x)
 
-        coord.append((k, a, fa, b, fb, x, fx, intervaloAtual))
+        intervaloAtual = abs(b - a)
 
         # checando se a diferenca do intervalo ja nao 
         # esta proxima o suficiente da tolerancia
         if(intervaloAtual <= epsilon):
             return(False, k, x, coord)
-        
-        k += 1
     
     print("Numero maximo de interacoes alcancado")
     return(True, k, x, coord)
@@ -66,17 +74,17 @@ def imprimir_resultado(statusErro, raiz, nIteracoes):
 
 def plotar(coordenadas):
     iterac = []
-    intervDelta = []
+    valorRaiz = []
 
     for i in coordenadas:
         iterac.append(i[0]) #posição 0, k
-        intervDelta.append(i[5]) #posição 7, x
+        valorRaiz.append(i[5]) #posição 5, x
 
-    pyplot.plot(iterac,intervDelta)
-    pyplot.title('Gráfico de Convergência - Método da Bisseção')
+    pyplot.plot(iterac,valorRaiz)
+    pyplot.title('Gráfico de Convergência - Método da Falsa Posição')
     pyplot.show()
 
-(haErro, iteracoes, raiz, coordenadas) = bissecao(f, -4, -2, 0.0001, 50)
+(haErro, iteracoes, raiz, coordenadas) = falsa_posicao(f, -4, -2, 0.0001, 50)
 
 imprimir_resultado(haErro, raiz, iteracoes)
 plotar(coordenadas)
