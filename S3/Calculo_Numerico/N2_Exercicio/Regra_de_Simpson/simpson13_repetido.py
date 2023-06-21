ac998506848793a67e033c05e846953945ef8556
1manuelc <manuelwn21@gmail.com>
2023-06-21 16:26:01 -0300

    feat/fix: simpson_repetido + exibicao/iteracao

--- S3/Calculo_Numerico/N2_Exercicio/Regra_de_Simpson/simpson13_repetido.py
+++ S3/Calculo_Numerico/N2_Exercicio/Regra_de_Simpson/simpson13_repetido.py
@@ -6,7 +6,7 @@ def f(x):
 def calcularErro(valor):
     erro = -47.9915748014691 - valor
     percentual = abs((erro * 100) / -47.9915748014691)
-    percentual = '~ {:.2f}%'.format(percentual)
+    percentual = '~ {:.5f}%'.format(percentual)
     return (erro, percentual)
 
 def imprimirResultado(coordenadas, resultadoFinal, erro):
@@ -19,19 +19,23 @@ def imprimirResultado(coordenadas, resultadoFinal, erro):
         xi.append(i[1])
         fxi.append(i[2])
     
-    print('\nTabela de Áreas usando Método do Trapézio Repetido\n')
+    print('\nTabela de Áreas usando Método de 1/3 de Simpson Repetido\n')
     print('Iteração\t Xi\t f(Xi)')
 
     for i in range(0, len(iteracoes)):
         print(iteracoes[i], '\t\t', '{:.2f}'.format(xi[i]), '\t', fxi[i])
 
     print('\nResultado final:', resultadoFinal)
-    print('Erro de', erro[0])
+    print('Erro de', '{:.15f}'.format(erro[0]))
     print('Corresponde a', erro[1], 'do resultado-alvo (-47.9915748014691)')
-    
-def trapezioRepetido(a, b, n):
-    intervaloH = (b - a) / n
-    
+
+def paridade(valor):
+    if(valor % 2 == 0): return 1
+    else: return 0
+
+def simpson13Repetido(a, b, m):
+    intervaloH = (b - a) / m
+
     coordenadas = []
     
     i = a
@@ -40,7 +44,8 @@ def trapezioRepetido(a, b, n):
 
     while(abs(i - b) > 1e-9):
         if(i == a): somatorio += f(i)
-        else: somatorio += 2 * f(i)
+        elif(paridade(contador) == 1): somatorio += 2 * f(i)
+        else: somatorio += 4 * f(i)
         
         coordenadas.append((contador, i, somatorio))
         i += intervaloH
@@ -49,10 +54,10 @@ def trapezioRepetido(a, b, n):
     somatorio += f(b)
     coordenadas.append((contador, i, somatorio))
 
-    somatorio *= (intervaloH / 2)
+    somatorio *= (intervaloH / 3)
     return (somatorio, coordenadas)
 
-resultado, coordenadas = trapezioRepetido(1, 3, 20)
+resultado, coordenadas = simpson13Repetido(1, 3, 20)
 valorErro = calcularErro(resultado)
 
 imprimirResultado(coordenadas, resultado, valorErro)
--- S3/Calculo_Numerico/N2_Exercicio/Regra_de_Simpson/simpson38_repetido.py
+++ S3/Calculo_Numerico/N2_Exercicio/Regra_de_Simpson/simpson38_repetido.py
@@ -6,7 +6,7 @@ def f(x):
 def calcularErro(valor):
     erro = -47.9915748014691 - valor
     percentual = abs((erro * 100) / -47.9915748014691)
-    percentual = '~ {:.2f}%'.format(percentual)
+    percentual = '~ {:.5f}%'.format(percentual)
     return (erro, percentual)
 
 def imprimirResultado(coordenadas, resultadoFinal, erro):
@@ -19,19 +19,23 @@ def imprimirResultado(coordenadas, resultadoFinal, erro):
         xi.append(i[1])
         fxi.append(i[2])
     
-    print('\nTabela de Áreas usando Método do Trapézio Repetido\n')
+    print('\nTabela de Áreas usando Método de 3/8 de Simpson Repetido\n')
     print('Iteração\t Xi\t f(Xi)')
 
     for i in range(0, len(iteracoes)):
         print(iteracoes[i], '\t\t', '{:.2f}'.format(xi[i]), '\t', fxi[i])
 
     print('\nResultado final:', resultadoFinal)
-    print('Erro de', erro[0])
+    print('Erro de', '{:.15f}'.format(erro[0]))
     print('Corresponde a', erro[1], 'do resultado-alvo (-47.9915748014691)')
-    
-def trapezioRepetido(a, b, n):
-    intervaloH = (b - a) / n
-    
+
+def paridade(valor):
+    if(valor % 2 == 0): return 1
+    else: return 0
+
+def simpson38Repetido(a, b, m):
+    intervaloH = (b - a) / m
+
     coordenadas = []
     
     i = a
@@ -40,7 +44,8 @@ def trapezioRepetido(a, b, n):
 
     while(abs(i - b) > 1e-9):
         if(i == a): somatorio += f(i)
-        else: somatorio += 2 * f(i)
+        elif(contador == 3): somatorio += (2 * f(i))
+        else: somatorio += (3 * f(i))
         
         coordenadas.append((contador, i, somatorio))
         i += intervaloH
@@ -49,10 +54,10 @@ def trapezioRepetido(a, b, n):
     somatorio += f(b)
     coordenadas.append((contador, i, somatorio))
 
-    somatorio *= (intervaloH / 2)
+    somatorio *= 3 * (intervaloH / 8)
     return (somatorio, coordenadas)
 
-resultado, coordenadas = trapezioRepetido(1, 3, 20)
+resultado, coordenadas = simpson38Repetido(1, 3, 15)
 valorErro = calcularErro(resultado)
 
 imprimirResultado(coordenadas, resultado, valorErro)
