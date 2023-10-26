acfddd90395898a354e505a77ae47c68c8934fdb
1manuelc <manuelwn21@gmail.com>
2023-10-26 13:25:13 -0300

    feat: S4/Topicos/Atv08_04etapa02

---
+++ S4/Topicos_especiais_em_Programacao/Atividade08_04etapa02/atv04_questao02/src/main/java/manuelc/atv04_questao02/Atv04_questao02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package S4.Topicos_especiais_em_Programacao.Atividade08_04etapa02.atv04_questao02.src.main.java.manuelc.atv04_questao02;
+
+/**
+ *
+ * @author manuelc
+ */
+public class Atv04_questao02 {
+    public static void main(String[] args) {
+        CalculoIMC calculadora = new CalculoIMC();
+        calculadora.setVisible(true);
+    }
+}
---
+++ S4/Topicos_especiais_em_Programacao/Atividade08_04etapa02/atv04_questao03/src/main/java/manuelc/atv04_questao03/Atv04_questao03.java
@@ -0,0 +1,12 @@
+package S4.Topicos_especiais_em_Programacao.Atividade08_04etapa02.atv04_questao03.src.main.java.manuelc.atv04_questao03;
+
+/**
+ *
+ * @author manuelc
+ */
+public class Atv04_questao03 {
+    public static void main(String[] args) {
+        LerVogal leitor = new LerVogal();
+        leitor.setVisible(true);
+    }
+}
