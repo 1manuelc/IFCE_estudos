ecf09d4d04945115e9cb1d8dd14b2658d3de03fe
1manuelc <manuelwn21@gmail.com>
2023-10-26 13:30:27 -0300

    refactor: Atv08_04etapa02 + enunciados

--- S4/Topicos_especiais_em_Programacao/Atividade08_04etapa02/atv04_questao02/src/main/java/manuelc/atv04_questao02/Atv04_questao02.java
+++ S4/Topicos_especiais_em_Programacao/Atividade08_04etapa02/atv04_questao02/src/main/java/manuelc/atv04_questao02/Atv04_questao02.java
@@ -1,6 +1,8 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+* #2. Construa uma aplicação que informe o IMC tomando como base a altura e o peso.
+* A aplicação deve possuir pelo menos os botões (calcular e limpar);
+* Obs2.: Utilizar a tabela abaixo para os retornos dos prognósticos (tabela de classificação do IMC).
+* */
 
 package S4.Topicos_especiais_em_Programacao.Atividade08_04etapa02.atv04_questao02.src.main.java.manuelc.atv04_questao02;
 
