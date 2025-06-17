6b238f4f2caa56b53e7a261478bca4e573b1b1d1
1manuelc <manuelwn21@gmail.com>
2025-06-17 11:55:45 -0300

    fix: "cannont" para "cannot"

--- S7/Topicos_Especiais_Em_Engenharia_De_Software/atv-01-testes-unitarios/src/q2-roman/q2-roman.ts
+++ S7/Topicos_Especiais_Em_Engenharia_De_Software/atv-01-testes-unitarios/src/q2-roman/q2-roman.ts
@@ -3,7 +3,7 @@ import { toArabic, toRoman } from 'roman-numerals';
 export class Roman {
 	romanize(num: number) {
 		if (num === 0) throw new Error('Error: cannot romanize zero');
-		if (num < 0) throw new Error('Error: cannont romanize negative number');
+		if (num < 0) throw new Error('Error: cannot romanize negative number');
         if (num > 3999) throw new Error('Error: cannot romanize numbers above 3999');
 
 		return toRoman(num);
